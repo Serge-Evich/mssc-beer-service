f0972cec9c7f048fa630ab6aa62c3a49a36b4721
SergeEvich <frenkdrebin@ukr.net>
2020-01-07 16:37:07 +0200

    Added BearRepository.

---
+++ src/main/java/microserivces/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package microserivces.msscbeerservice.repositories;
+
+import microserivces.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
